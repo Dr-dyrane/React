2d249edb19006a7a2575cdfd4dff56e668635378
Dr-dyrane <115133529+Dr-dyrane@users.noreply.github.com>
2022-12-08 20:24:57 +0100

    update Employee.js

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,9 +2,10 @@ function Employee(props) {
   return (
     <>
     <h3>Employee {props.name}</h3>
-    <p>{props.role}</p>
-    </>
-    )
+    <p>
+      {props.role ? props.role : 'No role'}
+    </p> < />
+  )
 }
 
 export default Employee;
