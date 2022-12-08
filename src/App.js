b4bbbc8d267df1434c4b0c264d776b6795bd91f2
Dr-dyrane <115133529+Dr-dyrane@users.noreply.github.com>
2022-12-08 14:47:02 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        hello! dyrane 
+    </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
           Learn React
         </a>
-      </header>
+    </header>
     </div>
   );
 }
