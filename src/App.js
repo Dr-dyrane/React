c586909d1ae7c63e994247dcc00ca41861277cd5
Dr-dyrane <115133529+Dr-dyrane@users.noreply.github.com>
2022-12-08 20:11:02 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import Employee from './components/Employee'
 
 function App() {
+  const showEmployees = false;
   return (
     <div className="App">
+      {showEmployees ?
+      <>
         <Employee />
         <Employee />
         <Employee />
+      </>
+      :
+        <p>You cannot see the employees</p>
+      }
     </div>
   );
 }
