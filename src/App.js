65c7a08d28c92773872e3967e553b1c336162931
Dr-dyrane <115133529+Dr-dyrane@users.noreply.github.com>
2022-12-08 21:53:16 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     setRole] = useState('developer');
   const showEmployees = true;
   return (
-    <div style="background:red"class="App bg-red-300">
+    <div class="App bg-red-300">
       {showEmployees ? (
       <>
       <input type='text' onChange={(e) => {
