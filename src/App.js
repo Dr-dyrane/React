029857a3d76a95b9b518d1a4b6d79edba58bb51b
Dr-dyrane <115133529+Dr-dyrane@users.noreply.github.com>
2022-12-09 01:19:40 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -15,29 +15,32 @@ function App() {
       <input type='text' onChange={(e) => {
         setRole(e.target.value);
       }} />
-      <div class="flex flex-wrap">
+      <div class="flex flex-wrap justify-center">
         <Employee
-          name="Jason"
-          role="manager"
-          img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
+        name="Jason"
+        role="manager"
+        img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
         <Employee
-          name="Mabel"
-          role={role}
-          img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
+        name="Mabel"
+        role={role}
+        img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
         <Employee
-          name="Allison"
-          img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
+        name="Allison"
+        img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
         <Employee
-          name="Jason"
-          role="manager"
-          img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
+        name="Jason"
+        role="manager"
+        img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
         <Employee
-          name="Mabel"
-          role={role}
-          img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
+        name="Mabel"
+        role={role}
+        img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
         <Employee
-          name="Allison"
-          img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
+        name="Allison"
+        img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
+        <Employee
+        name="Allison"
+        img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
       </div> < />
     ): (<p>
 You cannot see the employees
