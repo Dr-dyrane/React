f52361bc1034dc762fb3d0214f704b9f341dabf5
Dr-dyrane <115133529+Dr-dyrane@users.noreply.github.com>
2022-12-10 17:49:04 +0100

    update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -80,20 +80,19 @@ export default function Header(props) {
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pt-2 pb-3">
               {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "no-underline bg-gray-900 text-white"
-                      : "no-underline text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block px-3 py-2 rounded-md text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
+                <NavLink
+                key={item.name}
+                to={item.href}
+                className={({ isActive }) => {
+                  return (
+                    "block px-3 py-2 rounded-md text-base font-medium no-underline " +
+                    (!isActive
+                      ? "text-gray-300 hover:bg-gray-700 hover:text-white"
+                      : "bg-gray-900 text-white")
+                  );
+                }}>
                   {item.name}
-                </Disclosure.Button>
+                  </NavLink>
               ))}
             </div>
           </Disclosure.Panel>
