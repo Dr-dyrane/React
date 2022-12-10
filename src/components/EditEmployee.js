8618b197c75c4aca9c086718d78ea93321bd4f74
Dr-dyrane <115133529+Dr-dyrane@users.noreply.github.com>
2022-12-10 01:24:24 +0100

    update EditEmployee.js

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -14,7 +14,7 @@ function EditEmployee() {
   return (
     <>
     <button onClick={handleShow} className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Update</button>
-    
+
     <Modal
       show={show}
       onHide={handleClose}
@@ -22,17 +22,26 @@ function EditEmployee() {
       keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Update Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          I will not close if you click outside me. Don't even try to press
-          escape key.
+          <form className="w-full max-w-sm">
+  <div className="md:flex md:items-center mb-6">
+    <div className="md:w-1/3">
+      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+        Full Name
+      </label>
+    </div>
+    <div className="md:w-2/3">
+      <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe"/>
+    </div>
+  </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary">Understood</Button>
+          <Button variant="primary">Update</Button>
         </Modal.Footer>
       </Modal> < />
   );
