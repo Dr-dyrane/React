38e27a2d9ef18760b7de53c3d9c805f335ee99d5
Dr-dyrane <115133529+Dr-dyrane@users.noreply.github.com>
2022-12-10 16:53:18 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import './index.css';
+import "./index.css";
 
-import Header from './components/Header';
+import Header from "./components/Header";
 
-import Employees from './pages/Employees';
-import Customers from './pages/Customers';
+import Employees from "./pages/Employees";
+import Customers from "./pages/Customers";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Header>
         <Routes>
-          <Route path='/employees' element={<Employees />} />
-          <Route path='/customers' element={<Customers />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/customers" element={<Customers />} />
         </Routes>
       </Header>
     </BrowserRouter>
