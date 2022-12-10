8fbe5602a2da47fbb674622cb3a3a979e238ce72
Dr-dyrane <115133529+Dr-dyrane@users.noreply.github.com>
2022-12-10 01:31:54 +0100

    update EditEmployee.js

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -29,11 +29,20 @@ function EditEmployee() {
             <div className="md:flex md:items-center mb-6">
               <div className="md:w-1/3">
                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">Full Name</label>
-              </div>
+      </div>
             <div className="md:w-2/3">
               <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe" />
-            </div>
-            </div>
+      </div>
+      </div>
+        <div className="md:flex md:items-center">
+    <div className="md:w-1/3"></div>
+    <div className="md:w-2/3">
+      <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+        Sign Up
+      </button>
+    </div>
+  </div>
+</form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
