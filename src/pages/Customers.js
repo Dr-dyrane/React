d38a2dc21c832d712e2238ef2f15c9f06bb56df4
Dr-dyrane <115133529+Dr-dyrane@users.noreply.github.com>
2022-12-10 16:43:13 +0100

    update Employees.js

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,170 +1,116 @@
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Employee from '../components/Employee';
+import AddEmployee from '../components/AddEmployee';
+import EditEmployee from '../components/EditEmployee';
 
-import { NavLink } from "react-router-dom";
+import {
+  useState
+} from 'react';
+import {
+  v4 as uuidv4
+} from 'uuid';
 
-const navigation = [
-  {
-    name: "Employees",
-    href: "/Employees",
-  },
-  {
-    name: "Customers",
-    href: "/Customers",
-  },
-  {
-    name: "Projects",
-    href: "/other",
-  },
-  {
-    name: "Calender",
-    href: "/other2",
-  },
-];
+function Employees() {
+  const [employees,
+    setEmployees] = useState([{
+      id: 1,
+      name: "Jason",
+      role: "manager",
+      img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
+    },
+      {
+        id: 2,
+        name: "Mabel",
+        role: "developer",
+        img: "https://images.pexels.com/photos/2225298/pexels-photo-2225298.jpeg",
+      },
+      {
+        id: 3,
+        name: "Allison",
+        role: "intern",
+        img: "https://images.pexels.com/photos/3831635/pexels-photo-3831635.jpeg",
+      },
+      {
+        id: 4,
+        name: "Deli",
+        role: "realtor",
+        img: "https://images.pexels.com/photos/2095582/pexels-photo-2095582.jpeg",
+      },
+      {
+        id: 5,
+        name: "Bella",
+        role: "carer",
+        img: "https://images.pexels.com/photos/3760583/pexels-photo-3760583.jpeg",
+      },
+      {
+        id: 6,
+        name: "Fortune",
+        role: "mariner",
+        img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg",
+      },
+    ]);
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id) {
+        return {
+          ...employee,
+          name: newName,
+          role: newRole
+        };
+      }
 
-export default function Header(props) {
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
+  function newEmployee(name, role, img) {
+    const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img,
+    }
+    setEmployees([
+      ...employees,
+      newEmployee
+    ])
+  }
+
+  const showEmployees = true;
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-14 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <NavLink
-                        key={item.name}
-                        to={item.href}
-                        className={({ isActive }) => {
-                          return (
-                            "px-3 py-2 rounded-md text-sm font-medium no-underline" +
-                            (!isActive
-                              ? "text-gray-300 hover:bg-gray-700 hover:text-white"
-                              : "bg-gray-900 text-white")
-                          );
-                        }}
-                      >
-                        {item.name}
-                      </NavLink>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="sr-only">Open user menu</span>
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
-            </div>
-          </div>
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pt-2 pb-3">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "no-underline bg-gray-900 text-white"
-                      : "no-underline text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block px-3 py-2 rounded-md text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-          {props.children}{" "}
-          <footer>
-            {" "}
-            <div className="text-white text-center">Example</div>{" "}
-          </footer>{" "}
-        </>
-      )}
-    </Disclosure>
+    <div className="App bg-gray-300 min-h-screen">
+      {showEmployees ? (
+      <>
+      <div className="flex flex-wrap justify-center my-2">
+        {employees.map((employee) => {
+        const editEmployee = (
+          <EditEmployee
+            id={employee.id}
+            name={employee.name}
+            role={employee.role}
+            updateEmployee={updateEmployee}
+            />
+        );
+        return (
+          <Employee
+            key={employee.id}
+            id={employee.id}
+            name={employee.name}
+            role={employee.role}
+            img={employee.img}
+            alt={employee.name}
+            editEmployee={editEmployee}
+            />);
+      })}
+      </div>
+      <AddEmployee newEmployee={newEmployee} /> < />
+    ): (<p>
+You cannot see the employees
+      </p>
+    )}
+    </div>
   );
 }
+
+export default Employees;
