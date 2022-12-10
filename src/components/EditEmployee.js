81947f6f8ed3b744c745ea4ff650f348106553b9
Dr-dyrane <115133529+Dr-dyrane@users.noreply.github.com>
2022-12-10 01:34:15 +0100

    update EditEmployee.js

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -32,17 +32,15 @@ function EditEmployee() {
       </div>
             <div className="md:w-2/3">
               <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe" />
-      </div>
-      </div>
-        <div className="md:flex md:items-center">
-    <div className="md:w-1/3"></div>
-    <div className="md:w-2/3">
-      <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-        Sign Up
-      </button>
-    </div>
-  </div>
-</form>
+            </div>
+            </div>
+            <div className="md:flex md:items-center">
+            <div className="md:w-1/3"></div>
+            <div className="md:w-2/3">
+              <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">Sign Up</button>
+            </div>
+            </div>
+          </form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -51,7 +49,7 @@ function EditEmployee() {
           <Button variant="primary">Update</Button>
         </Modal.Footer>
       </Modal> < />
-  );
+);
 }
 
 export default EditEmployee;
