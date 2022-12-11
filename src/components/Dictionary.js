045993f73f8d6eeaf516f5e5a0d289ed76b09686
Dr-dyrane <115133529+Dr-dyrane@users.noreply.github.com>
2022-12-11 02:25:00 +0100

    update Dictionary.js

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Dictionary() {
   const [word, setWord] = useState();
   
+  useEffect(() => {
+    console.log ('State Updated')
+  });
+
   return (
     <>
       <input
         type="text"
-        onChange ={(e) => {
-          setWord (e.target.value);
+        onChange={(e) => {
+          setWord(e.target.value);
         }}
       />
       <h1>Let's get the definition for {word}</h1>
     </>
-    )
+  );
 }
