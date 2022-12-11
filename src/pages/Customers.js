acf353deca7eb5dbbea98c4d08fbdb56df94374d
Dr-dyrane <115133529+Dr-dyrane@users.noreply.github.com>
2022-12-11 01:32:14 +0100

    update Customers.js

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,6 +1,6 @@
 export default function Customers() {
   return (
-    <div className="App bg-gray-300 min-h-screen">
+    <div className="App text-center bg-gray-300 min-h-screen">
       <h1>Hello Dear!</h1>
     </div>
   );
