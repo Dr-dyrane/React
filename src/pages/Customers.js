3087047cad1beebcbbd7715ccf9b6ba41f752bb6
Dr-dyrane <halodyrane@gmail.com>
2022-12-13 03:31:49 +0100

    link to django

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+
 export default function Customers() {
+  const [customers, setCustomers] = useState();
+  useEffect(() => {
+    console.log("Fetching...");
+    fetch("http://127.0.0.1:8000/api/customers/")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCustomers(data.customers);
+      });
+  }, []);
   return (
-    <div className="App text-center bg-gray-300 min-h-screen">
-      <h1>Hello Dear!</h1>
-    </div>
+    <>
+      <h1>Here are our customers:</h1>
+      {customers
+        ? customers.map((customer) => {
+            return <p>{customer.name}</p>;
+          })
+        : null}
+    </>
   );
 }
