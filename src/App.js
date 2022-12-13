bb9e71bba80853a17bcc5f6dc141f52598c050bc
Dr-dyrane <halodyrane@gmail.com>
2022-12-13 13:08:23 +0100

    end-link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Dictionary from "./pages/Dictionary";
 import Definition from "./pages/Definition";
 import Employees from "./pages/Employees";
 import Customers from "./pages/Customers";
+import Customer from "./pages/Customer";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="/dictionary" element={<Dictionary />} />
           <Route path="/dictionary/:search" element={<Definition />} />
           <Route path="/customers" element={<Customers />} />
+          <Route path="/customers/:id" element={<Customer />} />
           <Route path="/404" element={<NotFound />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
